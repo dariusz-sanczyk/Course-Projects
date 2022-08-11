8f1cf0c589c590459932deabec233edb7c09d185
Darek Sańczyk <tantyk@gmail.com>
2022-08-11 23:12:26 +0200

    buttons functionality and player switching

--- Pig game/script.js
+++ Pig game/script.js
@@ -4,7 +4,12 @@ const rollMachine = function () {
   let newRoll = Math.trunc(Math.random() * 6 + 1);
   return newRoll;
 };
+let score0 = 0;
+let score1 = 0;
+let currScore = 0;
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const scorePlayer0El = document.getElementById('score--0');
 const scorePlayer1El = document.getElementById('score--1');
 const currentScorePlayer0El = document.getElementById('current--0');
@@ -14,17 +19,54 @@ const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 const newGameBtn = document.querySelector('.btn--new');
 
-let rollNumb = rollMachine;
-
-const roll = function () {
-  const roll = rollMachine();
-  console.log(roll);
-  diceEl.setAttribute('src', 'dice-' + roll + '.png');
-  diceEl.classList.remove('hidden');
+const currPLayer = function () {
+  const player = player0El.classList.contains('player--active') ? 0 : 1;
+  return player;
 };
 
 diceEl.classList.add('hidden');
 scorePlayer0El.textContent = 0;
 scorePlayer1El.textContent = 0;
 
+const displayScore = function (score, player) {
+  document.getElementById('score--' + player).textContent = score;
+};
+const displayCurrScore = function (score, player) {
+  document.getElementById('current--' + player).textContent = score;
+};
+
+const switchPlayer = function () {
+  if (player0El.classList.contains('player--active')) {
+    player0El.classList.remove('player--active');
+    player1El.classList.add('player--active');
+  } else {
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+  }
+};
+
+const roll = function () {
+  const roll = rollMachine();
+  console.log(roll);
+  diceEl.setAttribute('src', `dice-${roll}.png`);
+  diceEl.classList.remove('hidden');
+  if (roll !== 1) {
+    currScore += roll;
+    console.log(currScore);
+    displayCurrScore(currScore, currPLayer()); // if active player ? 0 : 1
+  } else {
+    currScore = 0;
+    displayCurrScore(currScore, currPLayer());
+    switchPlayer();
+  }
+};
+
+const hold = function () {
+  displayScore(currScore, currPLayer());
+  currScore = 0;
+  displayCurrScore(currScore, currPLayer());
+  switchPlayer();
+};
+
 rollBtn.addEventListener('click', roll);
+holdBtn.addEventListener('click', hold);
